909f49383c63763322efc8b82ee7805d0b258082
Al-Zahra <alzahra.almottahar@gmail.com>
2022-09-16 19:32:34 +0300

    Adding Tempreture Conversion

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ console.log(y);
 console.log(z);
 
 // defining x, y & z //
+
+
+
+let celsius = document.getElementById('celsius')
+let fahrenheit = document.getElementById('fahrenheit')
+
+celsius.oninput = () => {
+    let output = parseFloat(celsius.value /5) * 9 + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+fahrenheit.oninput = () => {
+    let output = parseFloat((fahrenheit.value -32) *5) /9;
+    celsius.value = parseFloat(output.toFixed(2));
+}
